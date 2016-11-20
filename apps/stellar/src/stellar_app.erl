91818a6f3c682aaf7d8f8322ae8b24465b7a22a8
Mijkenator <mijkenator@gmail.com>
2016-11-20 17:59:14 +0000

    remove active, replace it with own manual updater

--- apps/stellar/src/stellar_app.erl
+++ apps/stellar/src/stellar_app.erl
@@ -8,18 +8,46 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2, stop/1, update/0]).
 
 %%====================================================================
 %% API
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", stellar_root_handler, []}]}
+    ]),
+    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
     stellar_sup:start_link().
 
 %%--------------------------------------------------------------------
 stop(_State) ->
     ok.
+
+
+update() ->
+   Fn = fun(E) ->
+	case re:run(E, "lib/stellar-\\d.\\d.\\d/ebin$", [{capture, none}]) of
+		match -> true
+		;_    -> false
+	end
+   end,
+   [Path] = lists:filter(Fn, code:get_path()),
+   Fn1 = fun(File) ->
+	case re:run(File, "^(.+?)([^/]+).beam$") of
+	    {match, [_,{B,L},{B1,L1}]} ->
+		P = string:substr(File, B+1, L),
+		M = string:substr(File, B1+1, L1),
+		code:purge(list_to_existing_atom(M)),
+		LR = code:load_abs(P++M),
+		lager:debug("Reloaded ~p -> ~p", [M, LR])
+	    ;_ -> ok
+	end
+   end,
+   lists:foreach(Fn1, filelib:wildcard(Path ++ "/*.beam")).
 
 %%====================================================================
 %% Internal functions
---
+++ apps/stellar/src/stellar_root_handler.erl
@@ -0,0 +1,22 @@
+-module(stellar_root_handler).
+
+-export([init/3]).
+-export([handle/2]).
+-export([terminate/3]).
+-export([
+	ver/0
+]).
+
+init(_Type, Req, []) ->
+	{ok, Req, undefined}.
+
+handle(Req, State) ->
+	{ok, Req2} = cowboy_req:reply(200, [
+		{<<"content-type">>, <<"text/plain">>}
+	], <<"Hello world1!">>, Req),
+	{ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+	ok.
+
+ver() -> 6.
