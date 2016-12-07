84415a94d7e95c73b78db82f4e704bf1e64487a6
Mijkenator <mijkenator@gmail.com>
2016-12-07 03:19:25 +0000

    db auth

---
+++ apps/stellar/src/mkh_auth.erl
@@ -0,0 +1,20 @@
+-module(mkh_auth).
+
+-export([
+	login/2,
+	login/4
+]).
+
+
+login(User, Password) -> login(User, Password, <<>>, <<>>).
+login(Login, Password, SID, IP) ->
+    case emysql:execute(mysqlpool, <<"select u.id,u.utype from user u where login=? and password=?">>, [Login, Password]) of
+        {result_packet,_,_,[[Uid, UType]],_} when is_integer(Uid), Uid > 0 -> 
+		case SID of
+                    S when is_binary(S),size(S) > 5 -> 
+                        mkh_session:set_session(SID, [{<<"account_id">>, Uid},{<<"user_type">>, UType},{<<"ip">>, IP}])
+                    ;_ -> ok
+                end,
+		{ok, Uid, UType}
+        ;_                         -> 0 
+    end.	
