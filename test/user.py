9c21265238ddb3e95f490cf91dd956f05eac50fc
Mijkenator <mijkenator@gmail.com>
2016-12-13 01:58:29 +0000

    user test fix

--- test/user.py
+++ test/user.py
@@ -6,9 +6,9 @@ s = requests.Session()
 #url = "http://127.0.0.1:8080"
 url = "http://52.76.131.184/_mijkweb/user"
 
-#payload = {'request' : '{"type":"signup", "login":"user2", "password":"test"}'}
-#r = s.post(url, data=payload)
-#print(r.text)
+payload = {'request' : '{"type":"signup", "login":"mijkenator@gmail.com", "password":"test"}'}
+r = s.post(url, data=payload)
+print(r.text)
 
 #payload = {'request' : '{"type":"restore_password", "login":"mijkenator@gmail.com"}'}
 #r = s.post(url, data=payload)
@@ -18,7 +18,7 @@ url = "http://52.76.131.184/_mijkweb/user"
 #r = s.post(url, data=payload)
 #print(r.text)
 
-#exit(0)
+exit(0)
 
 #payload = {'request' : '{"type":"restore-pwd-confirm", "guid":"111111", "password":"test123"}'}
 payload = {'request' : '{"type":"delete", "login":"mijkenator@gmail.com", "password":"test123"}'}
