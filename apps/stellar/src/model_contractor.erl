1eb797e42ab981b5b03c6e959e445ceca297d4c0
Mijkenator <mijkenator@gmail.com>
2016-12-14 02:07:58 +0000

    api: /contractor signup fix user type

--- apps/stellar/src/model_contractor.erl
+++ apps/stellar/src/model_contractor.erl
@@ -5,7 +5,7 @@
 ]).
 
 signup(Login, Password, Refcode) ->
-	case emysql:execute(mysqlpool, <<"insert into contractor (login,password,refcode) values (?,?,?)">>, [Login,Password,Refcode]) of
+	case emysql:execute(mysqlpool, <<"insert into contractor (login,password,refcode,utype) values (?,?,?,2)">>, [Login,Password,Refcode]) of
 		{ok_packet,_,_,Uid,_,_,[]} -> {ok, Uid};
 		{error_packet,1,1062,<<"23000">>,_} -> {error, <<"already exists">>}
 	end.
