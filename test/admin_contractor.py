63401609ee7f5e344a739ee8f9f81276e97b530b
Mijkenator <mijkenator@gmail.com>
2016-12-16 17:11:11 +0000

    invite contractor test

---
+++ test/admin_contractor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import requests
+
+s = requests.Session()
+url = "http://52.76.131.184/_mijkweb/admin/service"
+
+payload = {'request' : '{"type":"get_categories", "login":"mijkenator@gmail.com", "password":"test"}'}
+r = s.post(url, data=payload)
+print(r.text)
+
+url = "http://52.76.131.184/_mijkweb/admin/contractor"
+payload = {'request' : '{"type":"invite_contractor", "email":"mijkenator@gmail.com"}'}
+r = s.post(url, data=payload)
+print(r.text)
+
