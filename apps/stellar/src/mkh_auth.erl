5dcc327f7044962e9201243c9618489be40b6934
Mijkenator <mijkenator@gmail.com>
2017-03-07 15:50:23 +0000

    nonconfirmed cannot login

--- apps/stellar/src/mkh_auth.erl
+++ apps/stellar/src/mkh_auth.erl
@@ -8,13 +8,18 @@
 
 login(User, Password) -> login(User, Password, <<>>, <<>>).
 login(Login, Password, SID, IP) ->
-    case emysql:execute(mysqlpool, <<"select u.id,u.utype from user u where login=? and password=?">>, [Login, Password]) of
-        {result_packet,_,_,[[Uid, UType]],_} when is_integer(Uid), Uid > 0 -> 
-		case SID of
-                    S when is_binary(S),size(S) > 5 -> 
-                        mkh_session:set_session(SID, [{<<"account_id">>, Uid},{<<"user_type">>, UType},{<<"ip">>, IP}])
-                    ;_ -> ok
-                end,
-		{ok, Uid, UType}
+    case emysql:execute(mysqlpool, <<"select u.id,u.utype,u.confirmed from user u where login=? and password=?">>, [Login, Password]) of
+        {result_packet,_,_,[[Uid, UType, Confirmed]],_} when is_integer(Uid), Uid > 0 -> 
+        case {UType, Confirmed} of
+          {0,0} -> 0;
+          {1,0} -> 0;
+          _     ->
+            case SID of
+                        S when is_binary(S),size(S) > 5 -> 
+                            mkh_session:set_session(SID, [{<<"account_id">>, Uid},{<<"user_type">>, UType},{<<"ip">>, IP}])
+                        ;_ -> ok
+                    end,
+            {ok, Uid, UType}
+        end
         ;_                         -> 0 
     end.	
